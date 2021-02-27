b3535a0e87dfba7d0548f5e1f26e6223cff3f0a3
James Park <jamespark0823@vt.edu>
2021-02-26 20:57:31 -0500

    sample demo of youtube api

--- node/app.js
+++ node/app.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express();
 
+var path = require('path')
+
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 40101;
 
 app.get('/', (req, res) => {
-	res.send('Hello World!');
+	res.sendFile(path.join(__dirname + '/example.html'));
 });
+
+app.get('activities', (req, res) => {
+	res.send(req);
+})
 
 app.listen(port, function() {
 	console.log('Server listening on port ' + port);
